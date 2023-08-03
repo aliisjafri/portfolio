a502eb8e036d8b43561b2f9ade585cdeefb99ca2
Ali <jafrimoali@gmail.com>
2023-08-02 18:23:46 -0700

    add copied url text changes

--- src/About/CopyButton.tsx
+++ src/About/CopyButton.tsx
@@ -7,11 +7,13 @@ const CopyButton = ({
   buttonText?: string
 }) => {
   const [isClicked, setIsClicked] = useState(false)
+  const [copied, setCopied] = useState(false)
   const handleCopyClick = async () => {
     setIsClicked(true)
     try {
       await navigator.clipboard.writeText(url)
       setIsClicked(false)
+      setCopied(true)
     } catch (err) {
       // eslint-disable-next-line no-console
       console.error('Failed to copy text: ', err)
@@ -23,10 +25,11 @@ const CopyButton = ({
     <button
       className={`absolute top-full left-1/2 mt-2 -translate-x-1/2 transform rounded bg-black px-3 py-1 text-sm text-white opacity-0 transition-all duration-200 group-hover:opacity-100 ${
         isClicked ? 'scale-75' : 'scale-100'
-      }`}
+      } ${copied ? 'font-extrabold' : ''}`}
+      onMouseLeave={() => setCopied(false)}
       onClick={handleCopyClick}
     >
-      Copy {buttonText}
+      {`${copied ? 'Copied' : 'Copy'} ${buttonText}${copied ? '!' : ''}`}
     </button>
   )
 }
