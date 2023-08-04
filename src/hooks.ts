f2ff21d3b24c84056cd47c643f8bf7264caf9a27
Ali <jafrimoali@gmail.com>
2023-08-03 18:08:15 -0700

    fix light theme flash

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,30 +1,21 @@
 import { useState, useEffect } from 'react'
 type Theme = 'dark' | 'light'
 
-const getInitialTheme = (): Theme => {
-  if (localStorage.getItem('theme')) {
-    return localStorage.getItem('theme') as Theme
-  } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    return 'dark'
-  } else {
-    return 'light'
-  }
-}
-
 export const useTheme = (): [Theme, () => void] => {
-  const [theme, setTheme] = useState<Theme>(getInitialTheme)
+  const [theme, setTheme] = useState<Theme>(() =>
+    document.documentElement.classList.contains('dark') ? 'dark' : 'light',
+  )
 
   useEffect(() => {
+    theme === 'dark'
+      ? document.documentElement.classList.remove('light')
+      : document.documentElement.classList.remove('dark')
+    document.documentElement.classList.add(theme)
     localStorage.setItem('theme', theme)
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
   }, [theme])
 
-  const toggleTheme = (): void => {
-    setTheme(previousTheme => (previousTheme === 'dark' ? 'light' : 'dark'))
+  const toggleTheme = () => {
+    setTheme(theme => (theme === 'dark' ? 'light' : 'dark'))
   }
 
   return [theme, toggleTheme]
