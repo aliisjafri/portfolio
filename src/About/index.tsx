dd8e79582474ce62e63cee14ca2ee7eb734da98f
Ali <jafrimoali@gmail.com>
2023-08-07 18:04:43 -0700

    remove hover shadow effect

--- src/About/index.tsx
+++ src/About/index.tsx
@@ -23,7 +23,7 @@ const About = () => (
     <img
       src="alijafriheadshot.jpeg"
       alt="Ali Jafri headshot"
-      className="mx-auto mt-6 h-32 w-32 transform rounded-3xl drop-shadow-[0_0_20px_#fef08a] transition-all duration-200 hover:drop-shadow-[0_0_35px_#fef08a]  dark:drop-shadow-[0_0_20px_#fbbf24] dark:hover:drop-shadow-[0_0_35px_#fbbf24] sm:h-48 sm:w-48 2xl:h-64 2xl:w-64"
+      className="mx-auto mt-6 h-32 w-32 transform rounded-3xl drop-shadow-[0_0_20px_#fef08a] transition-all duration-200 dark:drop-shadow-[0_0_20px_#fbbf24] sm:h-48 sm:w-48 2xl:h-64 2xl:w-64"
     />
     <div className="m-2 mt-8 flex justify-center">
       <h1 className="max-w-3xl text-4xl font-extrabold tracking-tight text-yellow-200 dark:text-amber-400 sm:text-5xl 2xl:text-6xl">
