63c5f986194b1330c44a76457bc26dd490f5a80e
Ali <jafrimoali@gmail.com>
2023-08-12 15:43:28 -0700

    add scaling on click to theme toggle

--- src/Components/ThemeToggleButton.tsx
+++ src/Components/ThemeToggleButton.tsx
@@ -39,6 +39,7 @@ const ThemeToggleButton = () => {
       initial={theme}
       animate={theme}
       whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.9 }}
       transition={{ type: 'spring', stiffness: 400, damping: 10 }}
     >
       <i
