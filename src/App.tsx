4554605a36abdfb0200e49642029b24281330674
Ali Jafri <jafrimoali@gmail.com>
2025-06-29 14:43:13 -0700

    add active state for link

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link, useLocation } from 'react-router-dom'
+import { Routes, Route, NavLink } from 'react-router-dom'
 import Projects from './Projects'
 import Articles from './Articles'
 import About from './About'
@@ -7,62 +7,37 @@ import PlantarFasciitis from './PatientTemplates/PlantarFasciitis'
 import PatientTemplates from './PatientTemplates'
 import ThemeToggleButton from './Components/ThemeToggleButton'
 import AudioPlayer from './AudioPlayer'
+
 const App = () => {
-  const page = useLocation().pathname
   return (
     <div className="min-h-screen overflow-hidden bg-gradient-to-tr from-violet-800 to-blue-400 p-6 text-slate-700 antialiased dark:from-violet-800 dark:to-slate-900 dark:text-slate-400">
       <div className="App">
-        <div className="flex items-center justify-between pb-4 dark:text-slate-400">
-          <div className="flex">
-            <Link
+        <header className="flex justify-between items-center w-full">
+          <div className="flex items-center">
+            <NavLink
               to="/"
-              className="underline-animation font-extrabold text-yellow-200 dark:text-amber-400"
+              end
+              className={({ isActive }) =>
+                `underline-animation font-extrabold text-yellow-200 dark:text-amber-400 ${
+                  isActive ? 'underline-active' : ''
+                }`
+              }
             >
               AliJafri.com
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/audio-player"
-              className="underline-animation font-extrabold text-yellow-200 dark:text-amber-400 ml-3"
+              className={({ isActive }) =>
+                `underline-animation font-extrabold text-yellow-200 dark:text-amber-400 ml-3 ${
+                  isActive ? 'underline-active' : ''
+                }`
+              }
             >
               Audio Player
-            </Link>
+            </NavLink>
           </div>
-          <div className="flex gap-x-3">
-            <ThemeToggleButton />
-            {false && (
-              <>
-                <Link
-                  to="/"
-                  className={`${
-                    page === '/' ? 'text-yellow-200 dark:text-amber-400' : ''
-                  } underline-animation font-extrabold `}
-                >
-                  About
-                </Link>
-                <Link
-                  to="/projects"
-                  className={`${
-                    page === '/projects'
-                      ? 'text-yellow-200 dark:text-amber-400'
-                      : ''
-                  } underline-animation font-extrabold`}
-                >
-                  Projects
-                </Link>
-                <Link
-                  to="/articles"
-                  className={`${
-                    page === '/articles'
-                      ? 'text-yellow-200 dark:text-amber-400'
-                      : ''
-                  } underline-animation font-extrabold`}
-                >
-                  Articles
-                </Link>
-              </>
-            )}
-          </div>
-        </div>
+          <ThemeToggleButton />
+        </header>
         <Routes>
           <Route path="/">
             <Route index element={<About />} />
